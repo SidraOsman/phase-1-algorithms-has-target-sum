0e71294b57a9fd4e905db2f33fc23c39e2aad935
SidraOsman <osmansidra4@gmail.com>
2023-10-21 16:54:53 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function hasTargetSum(array, target) {
+  const seenNumbers = new Set();
+  for (const number of array ){
+    const complement  = target - number;
+    if (seenNumbers.has(complement) ){
+      return true;
+    }
+    seenNumbers.add(number);
+  }
+  return false;
   // Write your algorithm here
 }
 
